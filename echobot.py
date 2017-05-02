5a4b984564368d072c2931886778e558e1d5226e
asterlan <asterlan2@gmail.com>
2017-05-02 22:21:47 +0200

    Add files via upload

--- echobot.py
+++ echobot.py
@@ -2,7 +2,7 @@ import botogram
 import requests
 
 bot = botogram.create('376118437:AAHGUTat7yo67cQEKNweuWoVHPkjiCswfbk')
-url = "https://api.telegram.org/bot376118437:AAHGUTat7yo67cQEKNweuWoVHPkjiCswfbk/"
+#url = "https://api.telegram.org/bot376118437:AAHGUTat7yo67cQEKNweuWoVHPkjiCswfbk/"
 
 @bot.command("hello")
 def hello_command(chat, message, args):
