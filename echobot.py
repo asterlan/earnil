2d2b3c539f511d1e81d057943e4c793fd7f72916
asterlan <asterlan2@gmail.com>
2017-05-02 21:08:07 +0200

    Add files via upload

--- echobot.py
+++ echobot.py
@@ -1,6 +1,16 @@
 import botogram
+import os
 
 bot = botogram.create('376118437:AAHGUTat7yo67cQEKNweuWoVHPkjiCswfbk')
+
+TOKEN = '376118437:AAHGUTat7yo67cQEKNweuWoVHPkjiCswfbk'
+PORT = int(os.environ.get('PORT', '5000'))
+updater = Updater(TOKEN)
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.setWebhook("https://earnil.herokuapp.com/" + TOKEN)
+updater.idle()
 
 @bot.command("hello")
 def hello_command(chat, message, args):
