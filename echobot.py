df6e75e05c6d71b1dae24b51fe1fb8a11f92458f
asterlan <asterlan2@gmail.com>
2017-05-02 21:22:11 +0200

    Add files via upload

--- echobot.py
+++ echobot.py
@@ -1,16 +1,8 @@
 import botogram
-import os
+import requests
 
 bot = botogram.create('376118437:AAHGUTat7yo67cQEKNweuWoVHPkjiCswfbk')
-
-TOKEN = '376118437:AAHGUTat7yo67cQEKNweuWoVHPkjiCswfbk'
-PORT = int(os.environ.get('PORT', '5000'))
-updater = Updater(TOKEN)
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-updater.bot.setWebhook("https://earnil.herokuapp.com/" + TOKEN)
-updater.idle()
+url = "https://api.telegram.org/bot376118437:AAHGUTat7yo67cQEKNweuWoVHPkjiCswfbk/"
 
 @bot.command("hello")
 def hello_command(chat, message, args):
